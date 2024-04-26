189b021a2e13860d1a2ef9e12c27dc64ecfa71be
Carlos Rain <i@lovespite.com>
2024-04-26 16:56:06 +0800

    添加项目文件。

---
+++ CellAutomata/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CellAutomata
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            inputSpeed = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            inputSize = new NumericUpDown();
+            btnStartStop = new Button();
+            btnClear = new Button();
+            btnSave = new Button();
+            btnLoad = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)inputSpeed).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)inputSize).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.BackColor = Color.Black;
+            pictureBox1.Location = new Point(12, 45);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(919, 618);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+            // 
+            // inputSpeed
+            // 
+            inputSpeed.Increment = new decimal(new int[] { 100, 0, 0, 0 });
+            inputSpeed.Location = new Point(144, 11);
+            inputSpeed.Maximum = new decimal(new int[] { 2000, 0, 0, 0 });
+            inputSpeed.Minimum = new decimal(new int[] { 100, 0, 0, 0 });
+            inputSpeed.Name = "inputSpeed";
+            inputSpeed.Size = new Size(126, 23);
+            inputSpeed.TabIndex = 1;
+            inputSpeed.Value = new decimal(new int[] { 200, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 17);
+            label1.TabIndex = 2;
+            label1.Text = "Iteration speed (ms)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(288, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 17);
+            label2.TabIndex = 4;
+            label2.Text = "Cell size(pixel)";
+            // 
+            // inputSize
+            // 
+            inputSize.Increment = new decimal(new int[] { 2, 0, 0, 0 });
+            inputSize.Location = new Point(384, 11);
+            inputSize.Maximum = new decimal(new int[] { 11, 0, 0, 0 });
+            inputSize.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            inputSize.Name = "inputSize";
+            inputSize.Size = new Size(126, 23);
+            inputSize.TabIndex = 3;
+            inputSize.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            inputSize.ValueChanged += inputSize_ValueChanged;
+            // 
+            // btnStartStop
+            // 
+            btnStartStop.Location = new Point(521, 5);
+            btnStartStop.Name = "btnStartStop";
+            btnStartStop.Size = new Size(75, 33);
+            btnStartStop.TabIndex = 5;
+            btnStartStop.Text = "Start";
+            btnStartStop.UseVisualStyleBackColor = true;
+            btnStartStop.Click += btnStartStop_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(602, 5);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 33);
+            btnClear.TabIndex = 6;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(683, 5);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 33);
+            btnSave.TabIndex = 7;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(764, 5);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(75, 33);
+            btnLoad.TabIndex = 7;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(943, 675);
+            Controls.Add(btnLoad);
+            Controls.Add(btnSave);
+            Controls.Add(btnClear);
+            Controls.Add(btnStartStop);
+            Controls.Add(label2);
+            Controls.Add(inputSize);
+            Controls.Add(label1);
+            Controls.Add(inputSpeed);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Cell Automata";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)inputSpeed).EndInit();
+            ((System.ComponentModel.ISupportInitialize)inputSize).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private NumericUpDown inputSpeed;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown inputSize;
+        private Button btnStartStop;
+        private Button btnClear;
+        private Button btnSave;
+        private Button btnLoad;
+    }
+}
---
+++ CellAutomata/Form1.cs
@@ -0,0 +1,394 @@
+using System.Diagnostics;
+
+namespace CellAutomata;
+
+public partial class Form1 : Form
+{
+    private readonly CellEnvironment _env;
+
+    private readonly ViewWindow _view;
+    private Thread? _evolutionThread = null;
+    private CancellationTokenSource? _cts = null;
+
+    public int CellSize
+    {
+        get
+        {
+            return (int)inputSize.Value;
+        }
+        set
+        {
+            inputSize.Value = value;
+        }
+    }
+
+    public int Speed
+    {
+        get
+        {
+            return (int)inputSpeed.Value;
+        }
+        set
+        {
+            inputSpeed.Value = value;
+        }
+    }
+
+    public int ViewRealWidth
+    {
+        get
+        {
+            return pictureBox1.Width;
+        }
+    }
+
+    public int ViewWidth
+    {
+        get
+        {
+            return (int)Math.Ceiling((decimal)pictureBox1.Width / (decimal)CellSize);
+        }
+    }
+
+    public int ViewRealHeight
+    {
+        get
+        {
+            return pictureBox1.Height;
+        }
+    }
+
+    public int ViewHeight
+    {
+        get
+        {
+            return (int)Math.Ceiling((decimal)pictureBox1.Height / (decimal)CellSize);
+        }
+    }
+
+    public Form1()
+    {
+        InitializeComponent();
+        var envWidth = 10000;
+        var vw = ViewWidth;
+        var vh = ViewHeight;
+
+        var initLeft = envWidth / 2 - vw / 2;
+        var initTop = envWidth / 2 - vh / 2;
+
+        _env = new CellEnvironment(envWidth, envWidth);
+        _view = new ViewWindow(_env, vw, vh, CellSize);
+        _view.MoveTo(initLeft, initTop);
+
+        Resize += Form1_Resize;
+    }
+
+    private void Form1_Resize(object? sender, EventArgs e)
+    {
+        _view.Resize(ViewWidth, ViewHeight, CellSize);
+        pictureBox1.Invalidate();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private int _dragStartX;
+    private int _dragStartY;
+    private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Left)
+        {
+            var col = e.X / CellSize + _view.Left;
+            var row = e.Y / CellSize + _view.Top;
+
+            if (col < 0 || row < 0)
+            {
+                return;
+            }
+            if (col >= _env.Width || row >= _env.Height)
+            {
+                return;
+            }
+
+
+            _env.ToggleCell(row, col);
+            pictureBox1.Invalidate();
+        }
+        else if (e.Button == MouseButtons.Right)
+        {
+            _dragStartX = e.X;
+            _dragStartY = e.Y;
+            pictureBox1.Cursor = Cursors.SizeAll;
+        }
+    }
+
+    private void pictureBox1_Paint(object sender, PaintEventArgs e)
+    {
+        _view.Draw(e.Graphics);
+    }
+
+    private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Right)
+        {
+            var dx = -(e.X - _dragStartX) / CellSize / 4;
+            var dy = -(e.Y - _dragStartY) / CellSize / 4;
+            _view.Move(dx, dy);
+            pictureBox1.Invalidate();
+        }
+    }
+
+    private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Right)
+        {
+            pictureBox1.Cursor = Cursors.Default;
+        }
+    }
+
+    private void inputSize_ValueChanged(object sender, EventArgs e)
+    {
+        _view.Resize(ViewWidth, ViewHeight, CellSize);
+        pictureBox1.Invalidate();
+    }
+
+    private async Task ReDraw()
+    {
+        await Task.Run(() =>
+        {
+            Invoke((MethodInvoker)delegate
+            {
+                pictureBox1.Invalidate();
+            });
+        });
+    }
+
+    private void EvolutionThread()
+    {
+        while (_cts is not null && !_cts.IsCancellationRequested)
+        {
+            _env.Evolve();
+            _ = ReDraw();
+
+            Debug.WriteLine($"Generation: {_env.Generation}");
+            Thread.Sleep(Speed);
+        }
+
+        Debug.WriteLine("EvolutionThread stopped");
+    }
+
+
+    private void btnStartStop_Click(object sender, EventArgs e)
+    {
+        if (_evolutionThread is null)
+        {
+            Start();
+        }
+        else
+        {
+            Stop();
+        }
+    }
+
+    private void Stop()
+    {
+        if (_cts is null) return;
+
+        _cts.Cancel();
+        _evolutionThread!.Join();
+        _evolutionThread = null;
+        _cts = null;
+
+        btnStartStop.Text = "Start";
+        Debug.WriteLine("Stopped");
+    }
+
+    private void Start()
+    {
+        _cts = new CancellationTokenSource();
+        _evolutionThread = new Thread(EvolutionThread);
+        btnStartStop.Text = "Stop";
+        _evolutionThread.Start();
+        Debug.WriteLine("Started");
+    }
+
+    private void btnClear_Click(object sender, EventArgs e)
+    {
+        Stop();
+        _env.Clear();
+        pictureBox1.Invalidate();
+    }
+}
+
+
+public class ViewWindow
+{
+    private readonly CellEnvironment _cellEnvironment;
+
+    private int _width;
+    private int _height;
+    private int _cellSize;
+
+    private readonly Font _font = new("Arial", 12);
+
+    private int _left = 0;
+    private int _top = 0;
+
+    public int Left => _left;
+    public int Top => _top;
+
+    public void Resize(int width, int height, int cellSize)
+    {
+        _width = width;
+        _height = height;
+        _cellSize = cellSize;
+    }
+
+    public void MoveTo(int left, int top)
+    {
+        _left = left;
+        _top = top;
+
+        if (_left > _cellEnvironment.Width - _width)
+        {
+            _left = _cellEnvironment.Width - _width;
+        }
+
+        if (_top > _cellEnvironment.Height - _height)
+        {
+            _top = _cellEnvironment.Height - _height;
+        }
+
+        if (_left < 0)
+        {
+            _left = 0;
+        }
+
+        if (_top < 0)
+        {
+            _top = 0;
+        }
+
+    }
+
+    public void Move(int deltaX, int deltaY)
+    {
+        MoveTo(_left + deltaX, _top + deltaY);
+    }
+
+    public ViewWindow(CellEnvironment cellEnvironment, int width, int height, int cellSize)
+    {
+        _cellEnvironment = cellEnvironment;
+        _width = width;
+        _height = height;
+        _cellSize = cellSize;
+    }
+
+    public void Draw(Graphics graphics)
+    {
+        var bpc = _cellEnvironment.Bpc;
+        var totalRows = _cellEnvironment.Height;
+        var totalColumns = _cellEnvironment.Width;
+
+        var bitmap = new ByteArrayBitOperator(_cellEnvironment.CreateSnapshot());
+        var genText = $"Generation: {_cellEnvironment.Generation}";
+
+        graphics.Clear(Color.Black);
+        DrawMainView(graphics, bitmap, bpc);
+        DrawGenerationText(graphics, genText);
+        DrawThumbnail(graphics, bitmap, bpc, totalRows, totalColumns);
+    }
+
+    private void DrawThumbnail(Graphics graphics, ByteArrayBitOperator bitmap, BitPositionConvert bpc, int totalRows, int totalColumns)
+    {
+        var thumbWidth = 240;
+        var cellSize = 1;
+        var thumbHeight = totalRows * thumbWidth / totalColumns;
+
+
+        // top right corner
+        var thumbLeft = _width * _cellSize - thumbWidth - 10;
+        var thumbTop = 10;
+
+        var rect = new Rectangle(
+            x: thumbLeft,
+            y: thumbTop,
+            width: thumbWidth,
+            height: thumbHeight);
+
+        // draw thumbnail background
+        graphics.FillRectangle(Brushes.Black, rect);
+
+        // draw thumbnail border
+        graphics.DrawRectangle(Pens.White, rect);
+
+        // draw thumbnail cells
+        for (int row = 0; row < totalRows; row++)
+        {
+            for (int col = 0; col < totalColumns; col++)
+            {
+                var bPos = bpc.Transform(row, col);
+                var cell = bitmap.Get(ref bPos);
+
+                if (!cell) continue;
+
+                var calcX = thumbLeft + ((col / (float)totalColumns) * thumbWidth);
+                var calcY = thumbTop + ((row / (float)totalRows) * thumbHeight);
+
+                graphics.FillRectangle(Brushes.White, calcX, calcY, cellSize, cellSize);
+            }
+        }
+
+        // draw thumbnail view window
+        var thumbViewWidth = Math.Min(thumbWidth, _width / (float)totalColumns * thumbWidth);
+        var thumbViewHeight = Math.Min(thumbHeight, _height / (float)totalRows * thumbHeight);
+        var thumbViewLeft = thumbLeft + (_left / (float)totalColumns * thumbWidth);
+        var thumbViewTop = thumbTop + (_top / (float)totalRows * thumbHeight);
+
+        var thumbViewRect = new RectangleF(
+            x: thumbViewLeft,
+            y: thumbViewTop,
+            width: thumbViewWidth,
+            height: thumbViewHeight);
+
+        graphics.DrawRectangle(Pens.Red, thumbViewRect);
+
+    }
+
+    private void DrawMainView(Graphics graphics, ByteArrayBitOperator bitmap, BitPositionConvert bpc)
+    {
+        var cellSize = _cellSize;
+
+        for (int row = _top; row < _top + _height; row++)
+        {
+            for (int col = _left; col < _left + _width; col++)
+            {
+                if (row >= _cellEnvironment.Height || col >= _cellEnvironment.Width)
+                {
+                    graphics.FillRectangle(Brushes.Gray, (col - _left) * cellSize, (row - _top) * cellSize, cellSize, cellSize);
+                    continue;
+                }
+
+                var bPos = bpc.Transform(row, col);
+                var cell = bitmap.Get(ref bPos);
+
+                if (!cell) continue;
+
+                var calcX = (col - _left) * cellSize;
+                var calcY = (row - _top) * cellSize;
+
+                graphics.FillRectangle(Brushes.White, calcX, calcY, cellSize, cellSize);
+            }
+        }
+    }
+
+    private void DrawGenerationText(Graphics graphics, string genText)
+    {
+        var size = graphics.MeasureString(genText, _font);
+        var rect = new RectangleF(0, 0, size.Width, size.Height);
+        graphics.FillRectangle(Brushes.Black, rect);
+        graphics.DrawString(genText, _font, Brushes.White, 0, 0);
+    }
+}
